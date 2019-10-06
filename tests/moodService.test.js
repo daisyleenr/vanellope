123e42fdbdbeea9f64663bf579e9a8bca19c163f
Nara Lee <daisyleenr@gmail.com>
2019-10-07 00:56:07 +0900

    Add setup/teardown to test

--- tests/moodService.test.js
+++ tests/moodService.test.js
@@ -1,14 +1,29 @@
+import mysql from 'mysql2/promise';
 import MoodService from '../services/MoodService';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const USER_ID = 1;
+const USER_ID = 999;
 const TEST_DB_CONFIG = {
   host: process.env.TEST_DB_HOST,
   user: process.env.TEST_DB_USER,
   password: process.env.TEST_DB_PASSWORD,
   database: process.env.TEST_DB_NAME
 }
+
+beforeAll(async () => {
+  const pool = mysql.createPool(TEST_DB_CONFIG);
+  await pool.query('INSERT INTO `user` (`id`) VALUES (?)', [USER_ID]);
+  for(let i = 0; i < 5; i++){
+    await pool.query('INSERT INTO `mood` (`id`, `score`, `memo`, `user_id`) VALUES (?, ?, ?, ?)', [10000 + i, 7, 'test contents ' + i, USER_ID]);
+  }
+});
+
+afterAll(async () => {
+  const pool = mysql.createPool(TEST_DB_CONFIG);
+  await pool.query('DELETE FROM `mood` WHERE `user_id` = ?', USER_ID);
+  await pool.query('DELETE FROM `user` WHERE `id` = ?', USER_ID);
+});
 
 test('Find all moods by user id', async () => {
   const moodService = new MoodService(TEST_DB_CONFIG);
